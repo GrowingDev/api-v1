efd5517c84de85eda4b94463f3a1df3e6e1e0950
GrowingDeveloper <andreas.benz.privat@icloud.com>
2022-05-18 10:56:21 +0200

    edit cors

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const collectionRouter = require('./src/collection')
 const productRouter = require('./src/product')
 var cors = require('cors')
 
+const allowedOrigins = ['https://www.shop.andreasbenz.org', 'http://shop.andreasbenz.org']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 const app = express();
 const port = 4000;
 
 app.use(express.static('public'));
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(morgan('common', {immediate: true}))
